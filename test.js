9b92af0830fdba7b40f820edfa1ad6ec1240256a
SudheerVelpula04 <velpulasudheer0@gmail.com>
2026-01-04 11:15:03 +0530

    modified

--- test.js
+++ test.js
@@ -26,7 +26,7 @@ test("subtract 10 - 4 should equal 6", () => {
 });
 
 test("multiply 3 * 4 should equal 12", () => {
-  const result = multiply(3, 4);
+  const result = multiply(500, 4);
   if (result !== 12) throw new Error(`Expected 12, got ${result}`);
 });
 
